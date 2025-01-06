2d645d2dbc9f759a15de00b1c233a3561c5dd04b
Sky <82303914+skysheng7@users.noreply.github.com>
2025-01-06 12:29:16 -0800

    build: added new packages

--- scripts.py
+++
@@ -1,6 +0,0 @@
-from pycounts_skysheng.pycounts_skysheng import count_words
-from pycounts_skysheng.plotting import plot_words
-import matplotlib.pyplot as plt
-counts = count_words("zen.txt")
-fig = plot_words(counts, 10)
-plt.show()
