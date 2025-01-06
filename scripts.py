b707061890d6f18ca80403e796a48e8381555c0d
Sky <82303914+skysheng7@users.noreply.github.com>
2025-01-05 23:30:50 -0800

    build: remove upper bound on dependency versions

--- scripts.py
+++ scripts.py
@@ -3,6 +3,4 @@ from pycounts_skysheng.plotting import plot_words
 import matplotlib.pyplot as plt
 counts = count_words("zen.txt")
 fig = plot_words(counts, 10)
-# Save the figure as a PNG file
-output_path = "word_counts.png"
-fig.savefig(output_path, bbox_inches="tight")
+plt.show()
